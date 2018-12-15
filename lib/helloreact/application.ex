d38328de1501cadb0ac5ea3ddea9dd7186ca7e55
richard12511 <richardnew12511@gmail.com>
2018-12-15 13:57:18 -0600

    initial commit

---
+++ lib/helloreact/application.ex
@@ -0,0 +1,31 @@
+defmodule Helloreact.Application do
+  use Application
+
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Start the Ecto repository
+      supervisor(Helloreact.Repo, []),
+      # Start the endpoint when the application starts
+      supervisor(HelloreactWeb.Endpoint, []),
+      # Start your own worker by calling: Helloreact.Worker.start_link(arg1, arg2, arg3)
+      # worker(Helloreact.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Helloreact.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    HelloreactWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
